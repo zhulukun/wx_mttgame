da1c04689b5ccb39bffc31d97b32c41373e35c9e
zlk <1770195221@qq.com>
2015-12-19 18:15:22 +0800

    update user.php

--- application/views/bike.php
+++ application/views/bike.php
@@ -3,14 +3,15 @@
 <head>
 	<title></title>
 </head>
-<body
+<body>
 		<?php echo $userinfo['nickname'];?>
+				<?php print_r($data);?>
+
         <?php foreach ($userinfo as $item): ?>  
         	<?php print_r($item);?>
        		 <?php //echo $item['nickname'];?>
            <?php endforeach; ?>  
 
-<p></p>
 
 </body>
 </html>
