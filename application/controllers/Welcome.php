a9bed0eddd743383795cca7979926eccbff3b0e7
zlk <1770195221@qq.com>
2015-12-19 21:41:03 +0800

    update get.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -14,7 +14,6 @@ class Welcome extends CI_Controller {
     }
 	public function index()
 	{
-		$url=
-		$code=file_get_contents($url);
+		header("Location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx86ae866c6733ee72&redirect_uri=http%3a%2f%2fgame.mttsmart.com%2fget&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
 	}
 }
