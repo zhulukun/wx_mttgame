0bf75d7e0e4b336ece5049cff263652c0e7bfa62
zlk <1770195221@qq.com>
2015-12-21 20:47:18 +0800

    update user

--- application/controllers/Get.php
+++ application/controllers/Get.php
@@ -21,6 +21,7 @@ class Get extends CI_Controller {
 		if(isset($_GET['code'])!=$_SESSION['code'])
 		{
 			$this->session->set_userdata('nickname',$_GET['code']);
+			$code=$_GET['code'];
 			$url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx86ae866c6733ee72&secret=e0c1e0c57061910c57eea05bd672a3c1&code={$code}&grant_type=authorization_code";
 			$json=file_get_contents($url);
 
