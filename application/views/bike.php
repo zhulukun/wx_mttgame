d594c0619b7f26b9eaa1499eb262a4c8dc75a06a
zlk <1770195221@qq.com>
2015-12-26 19:21:24 +0800

    update

--- application/views/bike.php
+++ application/views/bike.php
@@ -3,7 +3,7 @@ require_once "jssdk.php";
  //error_reporting(0);  
 $jssdk = new JSSDK("wx86ae866c6733ee72", "e0c1e0c57061910c57eea05bd672a3c1");
 $signPackage = $jssdk->GetSignPackage();
-print_r($signPackage);
+// print_r($signPackage);
 ?>
 
 <!DOCTYPE html>
