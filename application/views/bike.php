7e2a0d0733408d9753e27b4b68350d2835e58813
zlk <1770195221@qq.com>
2015-12-26 18:59:27 +0800

    update

--- application/views/bike.php
+++ application/views/bike.php
@@ -3,7 +3,7 @@ require_once "jssdk.php";
  error_reporting(0);  
 $jssdk = new JSSDK("wx86ae866c6733ee72", "e0c1e0c57061910c57eea05bd672a3c1");
 $signPackage = $jssdk->GetSignPackage();
-// print_r($signPackage);
+print_r($signPackage);
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +31,11 @@ $signPackage = $jssdk->GetSignPackage();
             'onMenuShareAppMessage'
           ]
     });
+    wx.ready(function(){
+    alert('ready');
+　　// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+ 
+});
      wx.checkJsApi({
             jsApiList: [
                 'getLocation',
