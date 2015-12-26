6bf71746be6d8aec1943cc544580a95ccb2f36ab
zlk <1770195221@qq.com>
2015-12-26 17:30:17 +0800

    update

--- application/views/bike.php
+++ application/views/bike.php
@@ -15,20 +15,7 @@ print_r($signPackage);
       <script src="js/jquery-2.1.3.js"></script>
   <script src="js/index.js"></script>
   <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"> </script>
-  <script type="text/javascript">
 
-    wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '', // 必填，公众号的唯一标识
-    timestamp: , // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名，见附录1
-    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
-
-    
-
-  </script>
 	<title>骑行故事</title>
 	<style type="text/css">
 			body {
@@ -137,4 +124,71 @@ print_r($signPackage);
 	</div>
 
 </body>
+<script>
+    wx.config({
+        debug: false,
+        appId: '<?php echo $signPackage["appId"];?>',
+        timestamp: <?php echo $signPackage["timestamp"];?>,
+        nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+        signature: '<?php echo $signPackage["signature"];?>',
+        jsApiList: [
+            // 所有要调用的 API 都要加到这个列表中
+            'checkJsApi',
+            'openLocation',
+            'getLocation',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+          ]
+    });
+     wx.checkJsApi({
+            jsApiList: [
+                'getLocation',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ],
+            success: function (res) {
+                alert(JSON.stringify(res));
+            }
+        });
+     
+    wx.onMenuShareAppMessage({
+        //  title: '<?php echo $news['Title'];?>',
+          desc: '<?php echo $userinfo['content'];?>',
+      //    link: '<?php echo $news['Url'];?>',
+          imgUrl: '<?php echo $userinfo['headimgurl'];?>',
+          trigger: function (res) {
+            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            // alert('用户点击发送给朋友');
+          },
+          success: function (res) {
+             alert('已分享');
+          },
+          cancel: function (res) {
+            // alert('已取消');
+          },
+          fail: function (res) {
+            // alert(JSON.stringify(res));
+          }
+        });
+
+        wx.onMenuShareTimeline({
+          title: '<?php echo $userinfo['content'];?>',
+         // link: '<?php echo $news['Url'];?>',
+          imgUrl: '<?php echo $userinfo['headimgurl'];?>',
+          trigger: function (res) {
+            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            // alert('用户点击分享到朋友圈');
+          },
+          success: function (res) {
+            // alert('已分享');
+          },
+          cancel: function (res) {
+            // alert('已取消');
+          },
+          fail: function (res) {
+            // alert(JSON.stringify(res));
+          }
+        });
+</script>
+
 </html>
